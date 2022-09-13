49c2db4e2ad3b36014c722b07d3715d532d5d0ad
huoruiwen <ruiwen.rw@nonolive.com>
2022-09-13 18:59:55 +0800

    tree maps

---
+++ collections/treemaps/k_v.go
@@ -0,0 +1,12 @@
+/**
+ * @Author raven
+ * @Description
+ * @Date 2022/9/13
+ **/
+package treemaps
+
+// Key is a generic key type of the map
+type Key interface{}
+
+// Value is a generic value type of the map
+type Value interface{}
