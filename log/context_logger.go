17931ce8636592de78a15d9470e93159ed1cfb60
huoruiwen <ruiwen.rw@nonolive.com>
2022-12-07 11:43:19 +0800

    feat: optimize log

--- log/context_logger.go
+++ log/context_logger.go
@@ -11,37 +11,37 @@ import (
 )
 
 type contextLogger struct {
-	log *logrus.Logger
+	logRus *logrus.Logger
 }
 
 func (d *contextLogger) Error(ctx context.Context, msg string) {
-	d.log.WithContext(ctx).Error(msg)
+	d.logRus.WithContext(ctx).Error(msg)
 }
 func (d *contextLogger) Errorf(ctx context.Context, format string, args ...interface{}) {
-	d.log.WithContext(ctx).Errorf(format, args...)
+	d.logRus.WithContext(ctx).Errorf(format, args...)
 }
 func (d *contextLogger) Info(ctx context.Context, msg string) {
-	d.log.WithContext(ctx).Info(msg)
+	d.logRus.WithContext(ctx).Info(msg)
 }
 func (d *contextLogger) Infof(ctx context.Context, format string, arg ...interface{}) {
-	d.log.WithContext(ctx).Infof(format, arg...)
+	d.logRus.WithContext(ctx).Infof(format, arg...)
 }
 func (d *contextLogger) Warn(ctx context.Context, msg string) {
-	d.log.WithContext(ctx).Warn(msg)
+	d.logRus.WithContext(ctx).Warn(msg)
 }
 func (d *contextLogger) Warnf(ctx context.Context, format string, arg ...interface{}) {
-	d.log.WithContext(ctx).Warnf(format, arg...)
+	d.logRus.WithContext(ctx).Warnf(format, arg...)
 }
 
 func (d *contextLogger) Debug(ctx context.Context, msg string) {
-	d.log.WithContext(ctx).Debug(msg)
+	d.logRus.WithContext(ctx).Debug(msg)
 }
 func (d *contextLogger) Debugf(ctx context.Context, format string, arg ...interface{}) {
-	d.log.WithContext(ctx).Debugf(format, arg...)
+	d.logRus.WithContext(ctx).Debugf(format, arg...)
 }
 
 func BuildContextLogger() ILogger {
 	return &contextLogger{
-		log: getLogrus(),
+		logRus: getLogrus(),
 	}
 }
--- log/trace_logger.go
+++ log/trace_logger.go
@@ -13,38 +13,38 @@ import (
 const TraceIdField = "trace-id"
 
 type TraceLogger struct {
-	log *logrus.Logger
+	logRus *logrus.Logger
 }
 
 func (d *TraceLogger) Error(ctx context.Context, msg string) {
-	logrus.WithField(TraceIdField, getTraceId(ctx)).WithContext(ctx).Error(msg)
+	d.logRus.WithField(TraceIdField, getTraceId(ctx)).WithContext(ctx).Error(msg)
 }
 func (d *TraceLogger) Errorf(ctx context.Context, format string, args ...interface{}) {
-	logrus.WithField(TraceIdField, getTraceId(ctx)).WithContext(ctx).Errorf(format, args...)
+	d.logRus.WithField(TraceIdField, getTraceId(ctx)).WithContext(ctx).Errorf(format, args...)
 }
 func (d *TraceLogger) Info(ctx context.Context, msg string) {
-	logrus.WithField(TraceIdField, getTraceId(ctx)).WithContext(ctx).Info(msg)
+	d.logRus.WithField(TraceIdField, getTraceId(ctx)).WithContext(ctx).Info(msg)
 }
 func (d *TraceLogger) Infof(ctx context.Context, format string, arg ...interface{}) {
-	logrus.WithField(TraceIdField, getTraceId(ctx)).WithContext(ctx).Infof(format, arg...)
+	d.logRus.WithField(TraceIdField, getTraceId(ctx)).WithContext(ctx).Infof(format, arg...)
 }
 func (d *TraceLogger) Warn(ctx context.Context, msg string) {
-	logrus.WithField(TraceIdField, getTraceId(ctx)).WithContext(ctx).Warn(msg)
+	d.logRus.WithField(TraceIdField, getTraceId(ctx)).WithContext(ctx).Warn(msg)
 }
 func (d *TraceLogger) Warnf(ctx context.Context, format string, arg ...interface{}) {
-	logrus.WithField(TraceIdField, getTraceId(ctx)).WithContext(ctx).Warnf(format, arg...)
+	d.logRus.WithField(TraceIdField, getTraceId(ctx)).WithContext(ctx).Warnf(format, arg...)
 }
 
 func (d *TraceLogger) Debug(ctx context.Context, msg string) {
-	logrus.WithField(TraceIdField, getTraceId(ctx)).WithContext(ctx).Debug(msg)
+	d.logRus.WithField(TraceIdField, getTraceId(ctx)).WithContext(ctx).Debug(msg)
 }
 func (d *TraceLogger) Debugf(ctx context.Context, format string, arg ...interface{}) {
-	logrus.WithField(TraceIdField, getTraceId(ctx)).WithContext(ctx).Debugf(format, arg...)
+	d.logRus.WithField(TraceIdField, getTraceId(ctx)).WithContext(ctx).Debugf(format, arg...)
 }
 
 func BuildTraceLogger() ILogger {
 	return &TraceLogger{
-		log: getLogrus(),
+		logRus: getLogrus(),
 	}
 }
 func getTraceId(ctx context.Context) string {
